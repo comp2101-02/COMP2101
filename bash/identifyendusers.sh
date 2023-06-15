8726acb60d7f14cd1a12782a3b27ae3dadfc75d6
comp2101-02 <113528157+comp2101-02@users.noreply.github.com>
2023-06-15 10:43:46 -0400

    Create identifyendusers.sh

---
+++ bash/identifyendusers.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+# demo of working with a wordlist using the for construct
+
+# generate a list of usernames for end-user accounts
+
+users="$(cut -d: -f1,3 /etc/passwd)"
+for user in $users; do
+  username=$(cut -d: -f1 <<< $user)
+  userid=$(cut -d: -f2 <<< $user)
+  if [ $userid -gt 999 ]; then
+    if [ ! -v allusers ]; then
+      allusers="$username"
+     else
+      allusers+=" $username"
+    fi
+  fi
+done
+echo "Found these users: $allusers"
