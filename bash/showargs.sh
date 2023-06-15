6afa3f216be6a89e22742c58cdc0e372c59bd1ed
Dennis Simpson <comp2101-02@zonzorp.net>
2023-06-15 11:19:48 -0400

    demo of cmdline args

---
+++ bash/showargs.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+cat <<EOF
+
+the variable 1 has '$1' in it
+the variable 2 has '$2' in it
+the variable 3 has '$3' in it
+
+the variable 0 has '$0' in it
+
+interesting, innit?
+
+EOF
